492a55fa6df4ef2b49f90fe397f3d53b0a9a083a
Alex Smag <alexsmag@gmail.com>
2024-03-10 14:19:05 -0400

    Modified Main Window to contain data templates for different view models + configuration in app.xaml

--- Home Simulator/App.xaml.cs
+++ Home Simulator/App.xaml.cs
@@ -5,13 +5,33 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Home_Simulator.Stores;
+using Home_Simulator.ViewModels;
 
 namespace Home_Simulator
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
+        private readonly NavigationStore _navigationStore;
+
+        public App()
+        {
+            _navigationStore = new NavigationStore();
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            _navigationStore.CurrentViewModel = new StartUpViewModel(_navigationStore);
+
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel(_navigationStore)
+            };
+
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
