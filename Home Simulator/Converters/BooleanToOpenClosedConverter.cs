78bf64504ee6971c0608a639ff8d120aec64cc5f
Alex Smag <alexsmag@gmail.com>
2024-03-10 14:25:24 -0400

    Added all the converters for light, door, window and user states (for UI purposes)

---
+++ Home Simulator/Converters/BooleanToOpenClosedConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Home_Simulator.Converters
+{
+    [ValueConversion(typeof(bool), typeof(string))]
+    public class BooleanToOpenClosedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                return (bool)value ? "Open" : "Closed";
+            }
+            return "Closed";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is string && (string)value == "On";
+        }
+    }
+}
