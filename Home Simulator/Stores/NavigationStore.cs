22d4714ba7a90785d68755f5524071a7570ab004
Alex Smag <alexsmag@gmail.com>
2024-03-10 14:21:32 -0400

    added navigation store to navigate between different views and updating their corresponding viewmodels

---
+++ Home Simulator/Stores/NavigationStore.cs
@@ -0,0 +1,31 @@
+﻿using Home_Simulator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Simulator.Stores
+{
+    public class NavigationStore
+    {
+        private ViewModelBase _currentViewModel;
+
+        public ViewModelBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set
+            {
+                _currentViewModel = value;
+                OnCurrentViewModelChanged();
+            }
+        }
+
+        public event Action CurrentViewModelChanged;
+
+        private void OnCurrentViewModelChanged()
+        {
+            CurrentViewModelChanged?.Invoke();
+        }
+    }
+}
