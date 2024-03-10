24c1d9498cf834d2d69356a3e6bbd960d4e1facb
Alex Smag <alexsmag@gmail.com>
2024-03-10 14:34:59 -0400

    Added light permission manager model to give access to lights ON/OFF based off user type and location

---
+++ Home Simulator/Models/LightPermissionManager.cs
@@ -0,0 +1,85 @@
+﻿using Home_Simulator.Models.HouseModels;
+using Home_Simulator.Models.ProfileModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Simulator.Models
+{
+    public class LightPermissionManager
+    {
+
+
+
+        public void UpdateLightPermissionsForAllUsers(ObservableCollection<Room> rooms, User currentUser)
+        {
+            foreach (var room in rooms)
+            {
+
+                foreach (var light in room.Lights)
+                {
+                    light.UserHasAccess = false;
+                }
+            }
+
+            if (currentUser != null)
+            {
+                if (currentUser.CurrentLocation is Room currentRoom)
+                {
+                    if (currentUser.Type == UserType.Child || currentUser.Type == UserType.Guest)
+                    {
+                        foreach (var light in currentRoom.Lights)
+                        {
+                            light.UserHasAccess = true;
+                        }
+                    }
+
+                    else
+                    {
+                        if (currentUser.Type != UserType.Stranger)
+                        {
+                            foreach (var room in rooms)
+                            {
+                                foreach (var light in room.Lights)
+                                {
+                                    light.UserHasAccess = true;
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+                else if (currentUser.CurrentLocation == null)
+                {
+                    if (currentUser.Type == UserType.Mother || currentUser.Type == UserType.Father)
+                    {
+                        foreach (var room in rooms)
+                        {
+                            foreach (var light in room.Lights)
+                            {
+                                light.UserHasAccess = true;
+                            }
+                        }
+                    }
+
+                    else
+                    {
+                        foreach (var room in rooms)
+                        {
+                            foreach (var light in room.Lights)
+                            {
+                                light.UserHasAccess = false;
+                            }
+                        }
+                    }
+                }
+            }
+
+            
+        }
+    }
+}
