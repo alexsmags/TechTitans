38958ad15b0ce01e33d39044b379ff0234399e03
Mohamad Mounir Yassin <mounir2yassin@gmail.com>
2024-03-20 17:36:24 -0400

    Implemented IAirConditioningController in MockAirConditioningController

---
+++ Home Simulator/Models/HouseSimulationModels/MockAirConditioningController.cs
@@ -0,0 +1,35 @@
+namespace Home_Simulator.HouseSimulationModels
+{
+    public class MockAirConditioningController : IAirConditioningController
+    {
+        private double _insideTemperature;
+        private bool _isOn = true;
+
+        public MockAirConditioningController(double insideTemperature)
+        {
+            _insideTemperature = insideTemperature;
+        }
+
+        public void Shutdown()
+        {
+            _isOn = false;
+            Console.WriteLine("Air conditioning has been shut down.");
+        }
+
+        public double GetInsideTemperature()
+        {
+            return _insideTemperature;
+        }
+
+        public bool IsAirConditioningOn()
+        {
+            return _isOn;
+        }
+
+        public void TurnOn()
+        {
+            _isOn = true;
+            Console.WriteLine("Air conditioning has been turned on.");
+        }
+    }
+}
