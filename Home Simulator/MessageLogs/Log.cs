948761bd00031e863c7e497771d3543d63a0acae
patrickmac3 <patrickmaceachen9@gmail.com>
2024-03-24 23:07:13 -0400

    Added Log class. Adds logs to xaml LogView component and saves them to the SimulationLogs.txt file

---
+++ Home Simulator/MessageLogs/Log.cs
@@ -0,0 +1,36 @@
+﻿using Home_Simulator.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+
+namespace Home_Simulator.MessageLogs
+{
+    public class Log
+    {
+        private static string _filePath = @"..\..\MessageLogs\SimulationLogs.txt";
+
+        private SimulationViewModel _simulationViewModel;
+
+        public Log(SimulationViewModel simulationViewModel)
+        {
+            _simulationViewModel = simulationViewModel;
+        }
+
+        public void AddMessage(string message)
+        {
+            string log = $"{DateTime.Now} - {message}";
+           _simulationViewModel.LogMessages.Add(log);
+            using (StreamWriter sw = File.AppendText(_filePath))
+            {
+                sw.WriteLine(log);
+            }
+
+        }
+    } 
+
+}
